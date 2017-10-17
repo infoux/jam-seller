fb55a66e33117588681fbd3e8454549e3930032c
Unknown <emrdl7@gmail.com>
2017-10-17 15:50:21 +0900

    스크립트 수정

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function () {
 
-    $("header#header .depth1 h2 a").on("mouseenter", function () {
+
+    $("header#header .depth1 h2 a").mouseover(function () {
         $("header#header .depth1").removeClass("active");
         $(this).parent().parent().addClass("active");
     });
